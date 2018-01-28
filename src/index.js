8519b8da4cd79ddbda140eea1c41f861ecd15460
Linas Mickevicius <linasmnew@gmail.com>
2018-01-28 10:58:41 +0000

    refactored to make animation work in safari which behaves unexpectedly

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 const fadeIn = `
-  @keyframes gracefulImage-fade-in {
+  @keyframes gracefulimage {
     0%   { opacity: 0.25; }
-    50%  { opacity: 0.50; }
+    50%  { opacity: 0.5; }
     100% { opacity: 1; }
   }
 `;
@@ -16,6 +16,7 @@ class GracefulImage extends Component {
     super(props);
 
     let placeholder = null;
+
     if (IS_SVG_SUPPORTED) {
       let width = (this.props.style && this.props.style.width) ? this.props.style.width : this.props.width ? this.props.width : '200';
       let height = (this.props.style && this.props.style.height) ? this.props.style.height : this.props.height ? this.props.height : '150';
@@ -24,10 +25,10 @@ class GracefulImage extends Component {
     }
 
     this.state = {
-      imageWorks: false,
+      loaded: false,
       retryDelay: this.props.retry.delay,
       retryCount: 1,
-      placeholder: placeholder
+      placeholder: placeholder,
     };
   }
 
@@ -35,11 +36,10 @@ class GracefulImage extends Component {
     Creating a stylesheet to hold the fading animation
   */
   componentWillMount() {
-    let exists = document.head.querySelectorAll('[data-gracefulimage]');
+    const exists = document.head.querySelectorAll('[data-gracefulimage]');
 
     if (!exists.length) {
       const styleElement = document.createElement('style');
-      styleElement.type = 'text/css';
       styleElement.setAttribute('data-gracefulimage', 'exists');
 
       document.head.appendChild(styleElement);
@@ -54,10 +54,10 @@ class GracefulImage extends Component {
   componentDidMount() {
     // if user has not opted-out from retrying then handle re-trying
     if (!this.props.noRetry) {
-      let image = new Image();
+      const image = new Image();
 
       image.onload = () => {
-        this.setState({ imageWorks: true });
+        this.setState({ loaded: true });
       }
 
       image.onerror = () => {
@@ -65,8 +65,6 @@ class GracefulImage extends Component {
       }
 
       image.src = this.props.src;
-    } else {
-      return;
     }
   }
 
@@ -81,7 +79,7 @@ class GracefulImage extends Component {
     following the default / provided retry algorithm
   */
   handleImageRetries(image) {
-    this.setState({ imageWorks: false }, () => {
+    this.setState({ loaded: false }, () => {
 
       if (this.state.retryCount <= this.props.retry.count) {
 
@@ -114,43 +112,48 @@ class GracefulImage extends Component {
   }
 
   /*
-    - if user didn't want a placeholder OR SVG not supported AND image doesn't work then don't render anything
-    - if image failed to load then render the placeholder
-    - else the image worked so render the image
+    - If image hasn't yet loaded AND user didn't want a placeholder OR SVG not supported then don't render anything
+    - Else if image hasn't yet loaded and SVG is supported then render the SVG
+    - Else render the image
   */
   render() {
-    if ((this.props.noPlaceholder || !IS_SVG_SUPPORTED) && !this.state.imageWorks) return null;
+    if (!this.state.loaded && (this.props.noPlaceholder || !IS_SVG_SUPPORTED)) return null;
 
-    if (!this.state.imageWorks && !this.props.noPlaceholder && IS_SVG_SUPPORTED) {
+    if (!this.state.loaded && IS_SVG_SUPPORTED) {
       return (
         <img
-           src={this.state.placeholder}
-           className={this.props.className}
-           width={this.props.width}
-           height={this.props.height}
-           style={{backgroundColor: this.props.placeholderColor, ...this.props.style}}
-           alt={this.props.alt}
+          src={this.state.placeholder}
+          className={this.props.className}
+          width={this.props.width}
+          height={this.props.height}
+          style={{
+            background: `#eee url(${this.props.src}) 0 0 / 0 0 no-repeat`,
+            ...this.props.style
+          }}
+          alt={this.props.alt}
         />
-      );
-    } else {
-      return (
-          <img
-            src={this.props.src}
-            className={this.props.className}
-            width={this.props.width}
-            height={this.props.height}
-            style={{
-              animationDuration: '0.3s',
-              animationIterationCount: 1,
-              animationName: 'gracefulImage-fade-in',
-              animationTimingFunction: 'ease-in',
-              ...this.props.style,
-            }}
-            alt={this.props.alt}
-          />
       );
     }
 
+    if (this.state.loaded) {
+      return (
+        <img
+          src={this.props.src}
+          className={this.props.className}
+          width={this.props.width}
+          height={this.props.height}
+          style={{
+            animationName: 'gracefulimage',
+            animationDuration: '0.3s',
+            animationIterationCount: 1,
+            animationTimingFunction: 'ease-in',
+            background: `url(${this.props.src}) 0 0 / 100% 100% no-repeat`,
+            ...this.props.style
+          }}
+          alt={this.props.alt}
+        />
+      );
+    }
   }
 }
 
