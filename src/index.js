f4d84a0d34e4352cb328caf8098cd27d9417f994
Linas Mickevicius <linasmnew@gmail.com>
2018-01-29 19:06:51 +0000

    changed svg in image support check

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const fadeIn = `
   }
 `;
 
-const IS_SVG_SUPPORTED = !!(document.createElementNS && document.createElementNS('http://www.w3.org/2000/svg','svg').createSVGRect);
+const IS_SVG_SUPPORTED =  document.implementation.hasFeature("http://www.w3.org/TR/SVG11/feature#Image", "1.1");
 
 class GracefulImage extends Component {
   constructor(props) {
