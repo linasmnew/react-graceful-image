3bb8e5f1ef65c0561a942146e8adf12ebcb379a0
Linas Mickevicius <linasmnew@gmail.com>
2018-02-03 16:25:41 +0000

    added render and event listener tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import ReactDOM from 'react-dom';
 import Enzyme, { mount, shallow } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16';
 import GracefulImage from '../src';
@@ -6,6 +7,16 @@ import GracefulImage from '../src';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('react-graceful-image', () => {
+  it('should render without error', () => {
+    const props = {
+      src: 'https://linasmickevicius.com/images/browser.png',
+      width: '150',
+      height: '150',
+    };
+    const div = document.createElement('div');
+    ReactDOM.render(<GracefulImage {...props} />, div);
+  });
+
   it('should render SVG placeholder when image has not loaded', () => {
     const props = {
       src: 'https://linasmickevicius.com/images/browser.png',
@@ -90,5 +101,37 @@ describe('react-graceful-image', () => {
 
     shallowWrapper.setState({ loaded: true });
     expect(shallowWrapper.find('img').prop('className')).toEqual(props.className);
+  });
+
+  it('should register load, scroll, resize and gestureend event listeners', () => {
+    const props = {
+      src: 'https://linasmickevicius.com/images/browser.png',
+      width: '150',
+      height: '150',
+    };
+    const spy = jest.spyOn(window, 'addEventListener');
+    const component = shallow(<GracefulImage {...props} />);
+
+    expect(spy).toHaveBeenCalled();
+    expect(spy).toHaveBeenCalledTimes(4);
+    expect(spy.mock.calls[0][0]).toEqual('load');
+    expect(spy.mock.calls[1][0]).toEqual('scroll');
+    expect(spy.mock.calls[2][0]).toEqual('resize');
+    expect(spy.mock.calls[3][0]).toEqual('gestureend');
+    spy.mockClear();
+  });
+
+  it('should not register any event listeners when given noLazyLoad prop', () => {
+    const props = {
+      src: 'https://linasmickevicius.com/images/browser.png',
+      width: '150',
+      height: '150',
+      noLazyLoad: true,
+    };
+    const spy = jest.spyOn(window, 'addEventListener');
+    const component = shallow(<GracefulImage {...props} />);
+
+    expect(spy).toHaveBeenCalledTimes(0);
+    spy.mockClear();
   });
 });
